6a8fe87a8010d98a5d2a54807618a6803f730569
Abdel-Rhman ALi <abdelrahman.ali.9400@gmail.com>
2018-05-19 15:08:01 +0200

    Update Receiver.java

--- app/src/main/java/com/example/android/prayerNotifier/Service/Receiver.java
+++ app/src/main/java/com/example/android/prayerNotifier/Service/Receiver.java
@@ -6,25 +6,36 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.example.android.prayerNotifier.MainActivity;
+
 import java.util.Calendar;
+
+import static android.content.Context.ALARM_SERVICE;
 
 public class Receiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            //Schedule today's notifications
+            Intent serviceIntent = new Intent(context, NotificationService.class);
+            serviceIntent.setAction("scheduling");
+            context.startService(serviceIntent);
+
+            PendingIntent pendingIntent = PendingIntent.getService(context, 0, serviceIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+            //Schedule to start the NotificationService every day
             Calendar calendar = Calendar.getInstance();
             calendar.setTimeInMillis(System.currentTimeMillis());
             calendar.set(Calendar.HOUR_OF_DAY, 1);
             calendar.set(Calendar.MINUTE, 0);
 
-            Intent intent1 = new Intent(context, NotificationService.class);
-            intent1.setAction("scheduling");
-            context.startService(intent1);
-            PendingIntent pendingIntent = PendingIntent.getService(context, 5, intent1, 0);
+            if (calendar.getTimeInMillis() < System.currentTimeMillis()) {
+                calendar.add(Calendar.DAY_OF_YEAR, 1);
+            }
 
-            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
             if (alarmManager != null) {
-                alarmManager.setInexactRepeating(AlarmManager.RTC,
+                alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,
                         calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
             }
         }
